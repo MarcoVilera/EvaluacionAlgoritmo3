fefb689ff3fb15f603a3d6d5d636d4c9faa6337a
MarcoVilera <marco@PCMARCO>
2023-05-14 00:40:46 -0400

    feat: Agregados métodos de ordenar
    2 nuevos métodos para ordenar de manera ascendente y descendente
    
    [Issue # si es necesario]

--- src/ventana/ListaEnlazada.java
+++ src/ventana/ListaEnlazada.java
@@ -48,7 +48,7 @@ public class ListaEnlazada {
         } else {
             /* Si se elimina un nodo diferente al primero, 
             recorre la lista enlazada hasta llegar al nodo anterior al que se desea eliminar y enlaza ese nodo con el siguiente nodo
-            */
+             */
             int cont = 0;
             Nodo temp = head;
             while (cont < index - 1) {
@@ -59,6 +59,92 @@ public class ListaEnlazada {
         }
         // Decrementa el tamaño de la lista
         size--;
+    }
+
+    //Método para ordenar de mayor a menor la lista 
+    public void sortMayor() {
+        // Inicializar variables
+        Nodo nodoActual = head; // Nodo actual que se está comparando en el bucle exterior
+        boolean huboIntercambios = true; // Bandera que indica si se han hecho intercambios en la última iteración
+
+        // Recorrer la lista mientras haya nodos que no estén ordenados y se hayan hecho intercambios en la última iteración
+        while (nodoActual != null && huboIntercambios) {
+            huboIntercambios = false;
+
+            // Recorrer la lista comparando nodos adyacentes
+            Nodo nodoAnterior = head; // Nodo anterior al nodo actual en el bucle interior
+            while (nodoAnterior.getSiguiente() != null) {
+                Nodo nodoSiguiente = nodoAnterior.getSiguiente(); // Nodo siguiente al nodo anterior en el bucle interior
+
+                // Si el nodo anterior es menor que el nodo siguiente, intercambiar los nodos
+                if (nodoAnterior.getValor() < nodoSiguiente.getValor()) {
+                    // Intercambiar nodos
+                    Nodo nodoTemporal = nodoAnterior.getSiguiente();
+                    nodoAnterior.setNext(nodoSiguiente.getSiguiente());
+                    nodoSiguiente.setNext(nodoAnterior);
+
+                    // Actualizar referencias de los nodos involucrados en el intercambio
+                    if (nodoAnterior == head) {
+                        head = nodoSiguiente;
+                    } else {
+                        Nodo nodoAnteAnterior = head;
+                        while (nodoAnteAnterior.getSiguiente() != nodoAnterior) {
+                            nodoAnteAnterior = nodoAnteAnterior.getSiguiente();
+                        }
+                        nodoAnteAnterior.setNext(nodoSiguiente);
+                    }
+                    nodoAnterior = nodoSiguiente;
+                    huboIntercambios = true;
+                } else {
+                    nodoAnterior = nodoSiguiente;
+                }
+            }
+
+            // Mover al nodo siguiente para la próxima iteración
+            nodoActual = nodoActual.getSiguiente();
+        }
+    }
+
+    //Método para ordenar de menor a mayor la lista 
+    public void sortMenor() {
+        // Inicializar variables
+        Nodo nodoActual = head;
+        boolean huboIntercambios = true;
+
+        // Recorrer la lista mientras haya nodos que no estén ordenados y se hayan hecho intercambios en la última iteración
+        while (nodoActual != null && huboIntercambios) {
+            huboIntercambios = false;
+
+            // Recorrer la lista comparando nodos adyacentes
+            Nodo nodoAnterior = head;
+            while (nodoAnterior.getSiguiente() != null) {
+                Nodo nodoSiguiente = nodoAnterior.getSiguiente();
+
+                // Si el nodo anterior es mayor que el nodo siguiente, intercambiar los nodos
+                if (nodoAnterior.getValor() > nodoSiguiente.getValor()) {
+                    // Intercambiar nodos
+                    Nodo nodoTemporal = nodoSiguiente.getSiguiente();
+                    nodoSiguiente.setNext(nodoAnterior);
+                    nodoAnterior.setNext(nodoTemporal);
+                    if (nodoAnterior == head) {
+                        head = nodoSiguiente;
+                    } else {
+                        Nodo nodoAnteriorAnterior = head;
+                        while (nodoAnteriorAnterior.getSiguiente() != nodoAnterior) {
+                            nodoAnteriorAnterior = nodoAnteriorAnterior.getSiguiente();
+                        }
+                        nodoAnteriorAnterior.setNext(nodoSiguiente);
+                    }
+                    nodoAnterior = nodoSiguiente;
+                    huboIntercambios = true;
+                } else {
+                    nodoAnterior = nodoSiguiente;
+                }
+            }
+
+            // Mover al nodo siguiente para la próxima iteración
+            nodoActual = nodoActual.getSiguiente();
+        }
     }
 
     // Método para verificar si la lista está vacía
